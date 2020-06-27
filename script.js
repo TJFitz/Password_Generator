0540ad4379550d0e7983a063d9667459d4f47f12
TJFitz <tjfitz@comcast.net>
2020-06-27 19:06:07 -0400

    array fixes and beginning of functionality

--- script.js
+++ script.js
@@ -2,16 +2,76 @@
 var generateBtn = document.querySelector("#generate");
 
 // Variable creation
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var specialChar = ['!', '@', '#', '\$', %, '\^', '&', '\*' '\(', '\)', '-', '=';
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "="];
 var passwordLength = 8;
-var isLower = false;
 var isUpper = false;
 var isSpecial = false;
 
 // Creating password function
-function generatePassword() {}
+function generatePassword() {
+  var isLower = confirm("Would you like lowercase letters?");
+  if (isLower === true) {
+    alert("Lowercase letters will be included.");
+  } else {
+    alert("Lowercase letters will not be included.");
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
