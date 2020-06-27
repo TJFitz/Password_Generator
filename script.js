0d830273ec331c494d1eec9032a98d277d11d543
TJFitz <tjfitz@comcast.net>
2020-06-27 18:56:57 -0400

    setting up variables

---
+++ script.js
@@ -0,0 +1,24 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Variable creation
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialChar = ['!', '@', '#', '\$', %, '\^', '&', '\*' '\(', '\)', '-', '=';
+var passwordLength = 8;
+var isLower = false;
+var isUpper = false;
+var isSpecial = false;
+
+// Creating password function
+function generatePassword() {}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
